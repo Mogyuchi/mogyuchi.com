632ab74ec8404c735ba87b14020c93dc4c1f2c83
Mogyuchi <69337624+Mogyuchi@users.noreply.github.com>
2022-01-29 03:47:19 +0000

    🔍 Next SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,23 @@
+import { DefaultSeoProps } from "next-seo";
+
+const SEO: DefaultSeoProps = {
+  titleTemplate: "%s | Mogyuchi",
+  defaultTitle: "Mogyuchi",
+  description: "Official website of Mogyuchi.",
+  openGraph: {
+    type: "website",
+    locale: "ja_JP",
+    url: "https://mogyuchi.vercel.app/",
+    title: "もぎゅち",
+    description: "Official website of Mogyuchi.",
+    site_name: "Mogyuchi",
+    images: [{ url: "https://mogyuchi.com/assets/ogp.jpg" }],
+  },
+  twitter: {
+    handle: "@Mogyuchi",
+    site: "@Mogyuchi",
+    cardType: "summary_large_image",
+  },
+};
+
+export default SEO;
