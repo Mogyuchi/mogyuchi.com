1662d0e8dd3b698b79ad0026da0c7ceec438ca68
Mogyuchi <69337624+Mogyuchi@users.noreply.github.com>
2022-03-14 10:28:01 +0900

    ➕ add sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import type { ReactElement } from "react";
 import Layout from "../components/layout";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
